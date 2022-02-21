b3efaab7971dcc735bdb2a7e02cbf2a076bb71bc
aamir0143 <mdaamirreza00@gmail.com>
2022-02-21 10:19:18 +0530

    [Aamir] Add : Start the Game

---
+++ TicTocToeGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace TicTocToeGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTocToe Game");
+            TicTocGame game = new TicTocGame();//object
+            game.StartGame();//call the method
+            Console.WriteLine("play the game");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TicTocToeGame/TicTocGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TicTocToeGame
+{
+    public class TicTocGame
+    {
+        public void StartGame()
+        {
+            char[] board = new char[10];
+            {
+                for (int i = 1; i < 10; i++)
+                {
+                    board [i] = '0';
+                }
+            }
+        }
+    }
+}
